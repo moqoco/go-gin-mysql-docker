85dcaa1982990b12a20e01ed606a84653ba094eb
chicoweb0624 <79202599+chicoweb0624@users.noreply.github.com>
2021-10-20 23:15:22 +0900

    godotenv

--- app/main.go
+++ app/main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"log"
+	
 	"api/model"
+
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	// DB connection gom
+	// read env
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Print("Not Found .env file.")
+	}
+
+	// DB connection gorm
 	model.Conn()
 
 	r := gin.Default()
--- app/model/db.go
+++ app/model/db.go
@@ -1,8 +1,10 @@
 package model
 
 import (
+	"os"
 	"fmt"
 	"log"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -11,8 +13,13 @@ var DB *gorm.DB
 var err error
 
 func Conn() {
-
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", "root", "root", "db", "3306", "test_db")
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("MYSQL_USER_NAME"),
+		os.Getenv("MYSQL_USER_PASSWORD"),
+		os.Getenv("MYSQL_HOST"),
+		os.Getenv("MYSQL_PORT"),
+		os.Getenv("MYSQL_DATABASE"))
+		
 	DB, err = gorm.Open(mysql.New(mysql.Config{
 		DSN: dsn,
 	}), &gorm.Config{})
